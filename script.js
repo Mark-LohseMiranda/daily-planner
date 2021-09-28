cfb3c597071e68993714a5491282cf2cf976f153
Mark L-M <mark.lohsemiranda@gmail.com>
2021-09-28 09:54:15 -0700

    updated class and id

--- script.js
+++ script.js
@@ -2,22 +2,22 @@ var dateTime = $("#dateTime");
 var saveBtn = $(".saveBtn");
 
 var schedule = {
-        nine: "",
-        ten: "",
-        eleven: "",
-        twelve: "",
-        one: "",
-        two: "",
-        three: "",
-        four: "",
-        five: "",
-    }
+    nine: "",
+    ten: "",
+    eleven: "",
+    twelve: "",
+    one: "",
+    two: "",
+    three: "",
+    four: "",
+    five: "",
+}
 
 
 function displayDateTime() {
-  var displayTime = setInterval(function () {
-    var date = new Date();
-    var time = (new Date()).toTimeString().substr(0,8);
+    var displayTime = setInterval(function () {
+        var date = new Date();
+        var time = (new Date()).toTimeString().substr(0,8);
     var fullDate =
       date.toLocaleString("default", { weekday: "long" }) +
       ", " +
@@ -36,19 +36,71 @@ function getSchedule() {
     if (storedSchedule !== null) {
         schedule = storedSchedule;
         $.each(schedule, function(key, value) {
-            var display = $('.' + key);
+            var display = $('#' + key);
             display.text(value);
+
         })
     }
 }
 
+function colorize() {
+    // var colorizeTime = (new Date()).toTimeString().substr(0,2);
+    // var test = new Date();
+    // console.log(test.toLocaleString('en-GB'));
+    // var colorizeTime = 10;
+    // console.log(colorizeTime);
+
+    var colorizeTime = new Date();
+    var hours = colorizeTime.getHours();
+    console.log(hours);
+    
+    if (colorizeTime === '09') {
+        $('#nine').addClass('present');
+        $('#ten ,#eleven ,#twelve ,#one ,#two ,#three ,#four ,#five').addClass('future');
+    };
+    if (colorizeTime === '10') {
+        $('#ten').addClass('present');
+        $('#eleven ,#twelve ,#one ,#two ,#three ,#four ,#five').addClass('future');
+    };
+    if (colorizeTime === '11') {
+        $('#eleven').addClass('present');
+        $('#twelve ,#one ,#two ,#three ,#four ,#five').addClass('future');
+    };
+    if (colorizeTime === '12') {
+        $('#twelve').addClass('present');
+        $('#one ,#two ,#three ,#four ,#five').addClass('future');
+    };
+    if (colorizeTime === '01') {
+        $('#one').addClass('present');
+        $('#two ,#three ,#four ,#five').addClass('future');
+    };
+    if (colorizeTime === '02') {
+        $('#two').addClass('present');
+        $('#three ,#four ,#five').addClass('future');
+    };
+    if (colorizeTime === '03') {
+        $('#three').addClass('present');
+        $('#four ,#five').addClass('future');
+    };
+    if (colorizeTime === '04') {
+        $('#four').addClass('present');
+        $('#five').addClass('future');
+    };
+    if (colorizeTime === '05') {
+        $('#five').addClass('present');
+    };
+   
+    }
+
+
 displayDateTime();
 getSchedule();
+colorize();
 
 
 saveBtn.on('click', function(event) {
     var content = $(event.target).siblings("textarea").val();
-    var className = $(event.target).siblings("textarea").attr('class');
+    var className = $(event.target).siblings("textarea").attr('id');
     schedule[`${className}`] = content;
     localStorage.setItem("dailyPlanner", JSON.stringify(schedule));
 })
