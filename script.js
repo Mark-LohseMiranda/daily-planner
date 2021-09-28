28c29a86e7e8c2159b4c007397b6804cdad39a85
Mark L-M <mark.lohsemiranda@gmail.com>
2021-09-28 11:21:11 -0700

    updated format and changed a word

--- script.js
+++ script.js
@@ -2,22 +2,21 @@ var dateTime = $("#dateTime");
 var saveBtn = $(".saveBtn");
 
 var schedule = {
-    nine: "",
-    ten: "",
-    eleven: "",
-    twelve: "",
-    one: "",
-    two: "",
-    three: "",
-    four: "",
-    five: "",
-}
-
+  nine: "",
+  ten: "",
+  eleven: "",
+  twelve: "",
+  one: "",
+  two: "",
+  three: "",
+  four: "",
+  five: "",
+};
 
 function displayDateTime() {
-    var displayTime = setInterval(function () {
-        var date = new Date();
-        var time = (new Date()).toTimeString().substr(0,8);
+  var displayTime = setInterval(function () {
+    var date = new Date();
+    var time = new Date().toTimeString().substr(0, 8);
     var fullDate =
       date.toLocaleString("default", { weekday: "long" }) +
       ", " +
@@ -31,73 +30,70 @@ function displayDateTime() {
 }
 
 function getSchedule() {
-    var storedSchedule = JSON.parse(localStorage.getItem("dailyPlanner"));
+  var storedSchedule = JSON.parse(localStorage.getItem("dailyPlanner"));
 
-    if (storedSchedule !== null) {
-        schedule = storedSchedule;
-        $.each(schedule, function(key, value) {
-            var display = $('#' + key);
-            display.text(value);
-
-        })
-    }
+  if (storedSchedule !== null) {
+    schedule = storedSchedule;
+    $.each(schedule, function (key, value) {
+      var display = $("#" + key);
+      display.text(value);
+    });
+  }
 }
 
 function colorize() {
+  var colorizeTime = new Date();
+  colorizeTime = colorizeTime.toLocaleString("en-US", {
+    hour: "numeric",
+    hour12: false,
+  });
 
-
-    var colorizeTime = new Date();
-    colorizeTime = colorizeTime.toLocaleString('en-US', {hour: 'numeric', hour12: false});
-     
-    
-    if (colorizeTime === '09') {
-        $('#nine').addClass('present');
-        $('#ten ,#eleven ,#twelve ,#one ,#two ,#three ,#four ,#five').addClass('future');
-    };
-    if (colorizeTime === '10') {
-        $('#ten').addClass('present');
-        $('#eleven ,#twelve ,#one ,#two ,#three ,#four ,#five').addClass('future');
-    };
-    if (colorizeTime === '11') {
-        $('#eleven').addClass('present');
-        $('#twelve ,#one ,#two ,#three ,#four ,#five').addClass('future');
-    };
-    if (colorizeTime === '12') {
-        $('#twelve').addClass('present');
-        $('#one ,#two ,#three ,#four ,#five').addClass('future');
-    };
-    if (colorizeTime === '13') {
-        $('#one').addClass('present');
-        $('#two ,#three ,#four ,#five').addClass('future');
-    };
-    if (colorizeTime === '14') {
-        $('#two').addClass('present');
-        $('#three ,#four ,#five').addClass('future');
-    };
-    if (colorizeTime === '15') {
-        $('#three').addClass('present');
-        $('#four ,#five').addClass('future');
-    };
-    if (colorizeTime === '16') {
-        $('#four').addClass('present');
-        $('#five').addClass('future');
-    };
-    if (colorizeTime === '17') {
-        $('#five').addClass('present');
-    };
-   
-    }
-
+  if (colorizeTime === "09") {
+    $("#nine").addClass("present");
+    $("#ten ,#eleven ,#twelve ,#one ,#two ,#three ,#four ,#five").addClass(
+      "future"
+    );
+  }
+  if (colorizeTime === "10") {
+    $("#ten").addClass("present");
+    $("#eleven ,#twelve ,#one ,#two ,#three ,#four ,#five").addClass("future");
+  }
+  if (colorizeTime === "11") {
+    $("#eleven").addClass("present");
+    $("#twelve ,#one ,#two ,#three ,#four ,#five").addClass("future");
+  }
+  if (colorizeTime === "12") {
+    $("#twelve").addClass("present");
+    $("#one ,#two ,#three ,#four ,#five").addClass("future");
+  }
+  if (colorizeTime === "13") {
+    $("#one").addClass("present");
+    $("#two ,#three ,#four ,#five").addClass("future");
+  }
+  if (colorizeTime === "14") {
+    $("#two").addClass("present");
+    $("#three ,#four ,#five").addClass("future");
+  }
+  if (colorizeTime === "15") {
+    $("#three").addClass("present");
+    $("#four ,#five").addClass("future");
+  }
+  if (colorizeTime === "16") {
+    $("#four").addClass("present");
+    $("#five").addClass("future");
+  }
+  if (colorizeTime === "17") {
+    $("#five").addClass("present");
+  }
+}
 
 displayDateTime();
 getSchedule();
 colorize();
 
-
-saveBtn.on('click', function(event) {
-    var content = $(event.target).siblings("textarea").val();
-    var className = $(event.target).siblings("textarea").attr('id');
-    schedule[`${className}`] = content;
-    localStorage.setItem("dailyPlanner", JSON.stringify(schedule));
-})
-
+saveBtn.on("click", function (event) {
+  var content = $(event.target).siblings("textarea").val();
+  var className = $(event.target).siblings("textarea").attr("id");
+  schedule[`${className}`] = content;
+  localStorage.setItem("dailyPlanner", JSON.stringify(schedule));
+});
